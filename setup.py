c4cef2f05f71bf61ce4e2d20996b19fa2c86a89f
dashaignatenko <ignadash@gmail.com>
2025-06-01 17:53:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="OldUk_morph",
+    name="old_uk_parser",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
